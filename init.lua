1017d7556268bad4e2cc4608207b0ddfe20ee818
tushar <tushar@lovoj.com>
2024-12-16 13:04:27 +0530

    fixed to 13 branch

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.opt.swapfile = false -- Disable swap files
 vim.opt.foldmethod = "manual"
 vim.opt.relativenumber = false
 vim.opt.number = false
---
+++ lua/plugins/core.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "folke/lazy.nvim",
+    version = "*", -- Use the latest Lazy.nvim for managing plugins
+  },
+  {
+    "LazyVim/LazyVim",
+    version = "13.x", -- Explicitly use version 13.x of LazyVim
+  },
+}
