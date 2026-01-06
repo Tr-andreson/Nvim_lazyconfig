c95306939b5fef4e8aaf5b3eb788df2aa811fd53
Tushar Rawat <tusharrawat.trf@gmail.com>
2026-01-06 07:10:05 +0000

    add twilight in zen mode

--- lua/plugins/zenmode.lua
+++ lua/plugins/zenmode.lua
@@ -4,5 +4,21 @@ return {
     event = "VeryLazy",
     cmd = "ZenMode",
     keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+    opts = {
+      plugins = {
+        twilight = { enabled = false }, -- we will toggle manually
+      },
+      on_open = function()
+        require("twilight").enable()
+      end,
+      on_close = function()
+        require("twilight").disable()
+      end,
+    },
+  },
+  {
+    "folke/twilight.nvim",
+    event = "VeryLazy",
+    opts = {}, -- default options are fine, you can customize if needed
   },
 }
